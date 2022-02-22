d5bba0510c28531fa3f0644d6a846c687fc73d0c
aditya <vinchuaditya@gmail.com>
2022-02-22 15:28:42 +0530

    generic code for more than three paameters

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -8,8 +8,15 @@ namespace Generic
         public static void Main(string[] args)
         {
             TestMaximum<int> maxint = new TestMaximum<int>();
+            TestMaximum<float> maxfloat = new TestMaximum<float>();
+            TestMaximum<string> maxstring = new TestMaximum<string>();
 
-            Console.WriteLine("Max:" + maxint.MaximumNumber(1, 5, 3));
+            Console.WriteLine("Max:" + maxint.MaximumNumber(1, 5, 3,54));
+
+            Console.WriteLine("Max:" + maxfloat.MaximumNumber(1.4f, 5.5f, 3.5f, 5.4f));
+
+            Console.WriteLine("Max:" + maxstring.MaximumNumber("d", "w", "t", "r"));
+
             Console.ReadLine();
         }
     }
